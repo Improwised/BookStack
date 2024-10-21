36aa162919cb3bee173f6abd165850e13f95c0ab
Brij Mandaliya <brij.mandliya@improwised.com>
2024-10-21 12:50:19 +0530

    fix: lint-js

--- resources/js/components/tags-suggestions.js
+++ resources/js/components/tags-suggestions.js
@@ -6,7 +6,7 @@ import {Component} from './component';
  */
 export class TagsSuggestions extends Component {
 
-      setup() {
+    setup() {
         this.container = this.$el;
         this.input = this.$refs.input;
         this.suggestionsTagModel = this.$refs.suggestionsTagModel;
@@ -15,73 +15,72 @@ export class TagsSuggestions extends Component {
         this.selectedTags = this.$opts.selectedTags;
         this.tags = this.$refs.tags;
         this.setupListeners();
-      }
-    
-      setupListeners() {
+    }
+
+    setupListeners() {
         const tags = JSON.parse(this.allTags);
         this.input.addEventListener('click', () => {
-          const {value} = this.input;
-          if (value.length === 0) {
-            this.suggestionBox.classList.toggle('hidden', false);
-          }
+            const {value} = this.input;
+            if (value.length === 0) {
+              this.suggestionBox.classList.toggle('hidden', false);
+            }
         });
-      
+
         this.input.addEventListener('input', () => {
-          const {value} = this.input;
-          const suggestions = tags.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
-        
-          if (value.length > 0) {
-            if (suggestions.length > 0) {
-              this.updateSuggestions(suggestions);
+            const {value} = this.input;
+            const suggestions = tags.filter(item => item.name.toLowerCase().includes(value.toLowerCase()));
+
+            if (value.length > 0) {
+                if (suggestions.length > 0) {
+                    this.updateSuggestions(suggestions);
+                } else {
+                    this.input.parentNode.querySelector('.card').classList.toggle('hidden', true);
+                }
             } else {
-              this.input.parentNode.querySelector('.card').classList.toggle('hidden', true);
+                this.suggestionBox.classList.toggle('hidden', false);
+                this.updateSuggestions(tags);
             }
-          } else {
-            this.suggestionBox.classList.toggle('hidden', false);
-            this.updateSuggestions(tags);
-          }
         });
-      
+
         document.addEventListener('click', event => {
-          if (!this.input.contains(event.target)) {
-            this.suggestionBox.classList.add('hidden');
-          }
-        });
-      
-        this.tags.addEventListener('click', event => {
-          if (event.target.matches('input[type=checkbox][data-id]')) {
-            const checkbox = event.target;
-            const tagName = checkbox.getAttribute('data-id');
-            const isChecked = checkbox.checked;
-          
-            if (isChecked) {
-              this.input.value = tagName;
-              checkbox.closest('form').submit();
+            if (!this.input.contains(event.target)) {
+                this.suggestionBox.classList.add('hidden');
             }
+        });
+
+        this.tags.addEventListener('click', event => {
+            if (event.target.matches('input[type=checkbox][data-id]')) {
+                const checkbox = event.target;
+                const tagName = checkbox.getAttribute('data-id');
+                const isChecked = checkbox.checked;
+
+                if (isChecked) {
+                    this.input.value = tagName;
+                    checkbox.closest('form').submit();
+                }
           }
         });
-      }
-    
-      /**
+    }
+
+    /**
        * @param {Array} suggestions
        */
-      async updateSuggestions(suggestions) {
+    async updateSuggestions(suggestions) {
         this.tags.innerHTML = '';
         suggestions.forEach(element => {
-          const isTagSelected = JSON.parse(this.selectedTags).some(
-            item => item.toLowerCase() === element.name.toLowerCase(),
-          );
-        
-          if (!isTagSelected) {
-            const clone2 = this.suggestionsTagModel.cloneNode(true);
-            clone2.classList.toggle('hidden', isTagSelected);
-            clone2.querySelector('.toggle-switch').id = element.name;
-            clone2.querySelector('.label').innerHTML = element.name;
-            clone2.querySelector('input[type=checkbox]').checked = isTagSelected;
-            clone2.querySelector('input[type=checkbox]').setAttribute('data-id', element.name);
-            this.tags.append(clone2);
-          }
+            const isTagSelected = JSON.parse(this.selectedTags).some(
+                item => item.toLowerCase() === element.name.toLowerCase(),
+            );
+
+            if (!isTagSelected) {
+                const clone2 = this.suggestionsTagModel.cloneNode(true);
+                clone2.classList.toggle('hidden', isTagSelected);
+                clone2.querySelector('.toggle-switch').id = element.name;
+                clone2.querySelector('.label').innerHTML = element.name;
+                clone2.querySelector('input[type=checkbox]').checked = isTagSelected;
+                clone2.querySelector('input[type=checkbox]').setAttribute('data-id', element.name);
+                this.tags.append(clone2);
+            }
         });
-      }
-      
+    }
 }
