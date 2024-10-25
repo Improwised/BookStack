32045c22f929586fa28408b6c48220b6404fa3d1
Brij Mandaliya <brij.mandliya@improwised.com>
2024-10-25 10:58:58 +0530

    remove: unwanted code

--- lang/en/entities.php
+++ lang/en/entities.php
@@ -192,7 +192,6 @@ return [
     'chapters_permissions_success' => 'Chapter Permissions Updated',
     'chapters_search_this' => 'Search this chapter',
     'chapter_sort_book' => 'Sort Book',
-    'chapter_cover_image' => 'Chapter Cover Image',
 
     // Pages
     'page' => 'Page',
