805461955158aaabdca1df1e6b6f180f73fd9b24
Brij Mandaliya <brij.mandliya@improwised.com>
2024-12-04 19:26:54 +0530

    Fix Lint PHP

--- database/migrations/2024_12_04_130801_rename_decrypt_password_to_password_in_pages_table.php
+++ database/migrations/2024_12_04_130801_rename_decrypt_password_to_password_in_pages_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('pages', function (Blueprint $table) {
-            $table->renameColumn('decrypt_password','password');
+            $table->renameColumn('decrypt_password', 'password');
         });
     }
 
